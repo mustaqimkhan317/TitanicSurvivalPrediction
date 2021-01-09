99852f0d38dd4945e292854d3ee6a51e4c811bc2
mustaqimkhan317 <mustaqimkhan317@gmail.com>
2021-01-09 18:39:02 +0600

    Added the model file

---
+++ model.py
@@ -0,0 +1,14 @@
+import numpy as np 
+import pandas as pd 
+
+'''
+The goal of this file is to deploy the titanic into the web 
+'''
+
+# init method 
+def __init__(self, value):
+	self.value = value
+
+# sample method
+def clean_data():
+	print("This method will process the data")
